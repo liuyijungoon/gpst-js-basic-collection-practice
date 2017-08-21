6660b7e2d116e3e65ce7e19c5f8e6b6b60929b7d
liuyijungoon <1019205777@qq.com>
2017-08-21 14:18:34 +0800

    task10-section3-wenstorm

--- main/section-3/practice-1.js
+++ main/section-3/practice-1.js
@@ -1,23 +1,13 @@
 'use strict';
-
-function includes(collection, ch) {
-    for (let item of collection) {
-        if (item === ch) {
-            return true;
-        }
-    }
-    return false;
-}
-
 module.exports = function createUpdatedCollection(collectionA, objectB) {
     let result = [];
-    for (let item of collectionA) {
+    collectionA.forEach(function (item,index) {
         let key = item.key;
         let count = item.count;
-        if (includes(objectB.value, key)) {
-            count--;
-        }
+        if(objectB.value.some(function (element,index) {
+                return element===key;
+            })){ count--;}
         result.push({ key, count });
-    }
+    });
     return result;
-}
+};
